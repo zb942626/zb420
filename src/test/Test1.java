5732719cc039df1de59c26e6c10c731ec715b101
zhaobo <173849495@qq.com>
2021-05-06 17:07:03 +0800

    new

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,6 +4,7 @@ package test;
 public class Test1 {
     public static void main(String[] args) {
         int i = 0;
+
         while (i<=1000) {
             if (i % 2 == 1&&i%3==2&&i%4==3&&i%5==4&&i%6==5){
                 System.out.println(i);
